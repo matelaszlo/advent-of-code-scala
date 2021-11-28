fbe804e3ac7bc598c3caaf78404f846e7d9b9a0c
Máté László <mate.laszlo.237@gmail.com>
2021-11-28 14:57:35 +0000

    Generate puzzle map

--- build.sbt
+++ build.sbt
@@ -1,10 +1,13 @@
 lazy val root = (project in file("."))
   .settings(name := "Advent of Code Scala")
   .settings(moduleName := "advent-of-code-scala")
-  .settings(version := "0.1")
-  .settings(scalaVersion := "2.13.4")
+  .settings(version := "1.0")
+  .settings(scalaVersion := "2.13.7")
   .settings(libraryDependencies ++= dependencies)
 
-lazy val dependencies = Seq("org.scalatest" %% "scalatest"   % "3.2.3"  % "test")
+lazy val dependencies = Seq(
+  "org.scala-lang"  % "scala-reflect" % "2.13.7",
+  "org.scalatest"  %% "scalatest"     % "3.2.10"  % "test"
+)
 
 scalacOptions ++= Seq("-deprecation", "-feature")
--- src/main/scala/com/lmat/adventofcode/PuzzleRunner.scala
+++ src/main/scala/com/lmat/adventofcode/PuzzleRunner.scala
@@ -1,100 +1,30 @@
 package com.lmat.adventofcode
 
+import scala.util.Try
+
 object PuzzleRunner extends App {
   val year = args.headOption.map(_.toInt).getOrElse(2020)
-  val day  = args.lift(1).map(_.toInt).getOrElse(1)
-
-  def puzzleMap = Map(
-    // 2015
-    (2015, 1)  -> year2015.Day01,
-    (2015, 2)  -> year2015.Day02,
-    (2015, 3)  -> year2015.Day03,
-    (2015, 4)  -> year2015.Day04,
-    (2015, 5)  -> year2015.Day05,
-    (2015, 6)  -> year2015.Day06,
-    (2015, 7)  -> year2015.Day07,
-    (2015, 8)  -> year2015.Day08,
-    (2015, 9)  -> year2015.Day09,
-    (2015, 10) -> year2015.Day10,
-    (2015, 11) -> year2015.Day11,
-    (2015, 12) -> year2015.Day12,
-    (2015, 13) -> year2015.Day13,
-    (2015, 14) -> year2015.Day14,
-    (2015, 15) -> year2015.Day15,
-    (2015, 16) -> year2015.Day16,
-    (2015, 17) -> year2015.Day17,
-    (2015, 18) -> year2015.Day18,
-    (2015, 19) -> year2015.Day19,
-    (2015, 20) -> year2015.Day20,
-    (2015, 21) -> year2015.Day21,
-    (2015, 22) -> year2015.Day22,
-    (2015, 23) -> year2015.Day23,
-    (2015, 24) -> year2015.Day24,
-    (2015, 25) -> year2015.Day25,
-
-    // 2017
-    (2017, 1)  -> year2017.Day01,
-    (2017, 2)  -> year2017.Day02,
-    (2017, 3)  -> year2017.Day03,
-    (2017, 4)  -> year2017.Day04,
-    (2017, 5)  -> year2017.Day05,
-    (2017, 6)  -> year2017.Day06,
-    (2017, 7)  -> year2017.Day07,
-    (2017, 8)  -> year2017.Day08,
-    (2017, 9)  -> year2017.Day09,
-    (2017, 10) -> year2017.Day10,
-    (2017, 11) -> year2017.Day11,
-    (2017, 12) -> year2017.Day12,
-    (2017, 13) -> year2017.Day13,
-    (2017, 14) -> year2017.Day14,
-    (2017, 15) -> year2017.Day15,
-    (2017, 16) -> year2017.Day16,
-    (2017, 17) -> year2017.Day17,
-    (2017, 18) -> year2017.Day18,
-    (2017, 19) -> year2017.Day19,
-    (2017, 20) -> year2017.Day20,
-    (2017, 21) -> year2017.Day21,
-    (2017, 22) -> year2017.Day22,
-    (2017, 23) -> year2017.Day23,
-    (2017, 24) -> year2017.Day24,
-    (2017, 25) -> year2017.Day25,
-
-    // 2018
-    (2018, 1)  -> year2018.Day01,
-    (2018, 2)  -> year2018.Day02,
-    (2018, 3)  -> year2018.Day03,
-    (2018, 4)  -> year2018.Day04,
-    (2018, 5)  -> year2018.Day05,
-    (2018, 6)  -> year2018.Day06,
-    (2018, 7)  -> year2018.Day07,
-    (2018, 8)  -> year2018.Day08,
-    (2018, 9)  -> year2018.Day09,
-    (2018, 10) -> year2018.Day10,
-    (2018, 11) -> year2018.Day11,
-    (2018, 12) -> year2018.Day12,
-    (2018, 13) -> year2018.Day13,
-    (2018, 14) -> year2018.Day14,
-    (2018, 15) -> year2018.Day15,
-    (2018, 16) -> year2018.Day16,
-    (2018, 17) -> year2018.Day17,
-    (2018, 18) -> year2018.Day18,
-    (2018, 19) -> year2018.Day19,
-    (2018, 24) -> year2018.Day24,
-
-    // 2019
-    (2019, 1)  -> year2019.Day01,
-    (2019, 2)  -> year2019.Day02,
-    (2019, 3)  -> year2019.Day03,
-    (2019, 4)  -> year2019.Day04,
-    (2019, 5)  -> year2019.Day05,
-    (2019, 6)  -> year2019.Day06,
-
-    // 2020
-    (2020, 1)  -> year2020.Day01,
-    (2020, 7)  -> year2020.Day07
-  )
-
+  val day = args.lift(1).map(_.toInt).getOrElse(1)
+  lazy val puzzleMap = buildPuzzleMap(2015, 2021)
   run(puzzleMap, year, day)
+
+  def buildPuzzleMap(from: Int, to: Int): Map[(Int, Int), Puzzle[_, _, _, _, _, _]] = {
+    for {
+      year   <- from to to
+      day    <- 1 to 25
+      puzzle <- puzzleReference(year, day)
+    } yield ((year, day), puzzle)
+  }.toMap
+
+  // Maybe this should be a macro instead of reflection
+  def puzzleReference(year: Int, day: Int): Option[Puzzle[_, _, _, _, _, _]] = Try {
+    import scala.reflect.runtime.{universe => ru}
+    val runtimeMirror = ru.runtimeMirror(getClass.getClassLoader)
+    val path = s"com.lmat.adventofcode.year$year.Day${"%02d".format(day)}"
+    val staticModule = runtimeMirror.staticModule(path)
+    val reflectModule = runtimeMirror.reflectModule(staticModule)
+    reflectModule.instance.asInstanceOf[Puzzle[_, _, _, _, _, _]]
+  }.toOption
 
   def resource(year: Int, day: Int): String = s"$year/Day${"%02d".format(day)}.txt"
 
